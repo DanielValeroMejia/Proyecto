a8e3f6df89d3cc189dcd68dccd0fd24dc77b3d0c
DanielValeroMejia <134336921+DanielValeroMejia@users.noreply.github.com>
2025-04-11 19:57:23 +0000

    hoy

--- src/Componentes/Pokemon/index.jsx
+++ src/Componentes/Pokemon/index.jsx
@@ -1,12 +1,53 @@
 import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react';
 
 import './style.css'
 
 function Pokemon() {
 
+  const { name } = useParams(); 
+  const [datapoke, setDatapoke] = useState([]);
+  const [favoritos, setFavoritos] = useState([]);
+  const esFavorito = favoritos.some(p => p.id === datapoke.id);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(response => response.json())
+      .then(responseData => setDatapoke(responseData))
+      .catch(error => console.error("Error:", error));
+  }, [name]);
+
+  const toggleFavorito = () => {
+    if (esFavorito) {
+      setFavoritos(favoritos.filter(p => p.id !== datapoke.id));
+    } else {
+      setFavoritos([...favoritos, { id: datapoke.id, nombre: datapoke.name }]);
+    }
+  };
+
   return (
     <>
-    <h1>Pokemon</h1>
+    <div>
+      <img 
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${datapoke.id}.png`} 
+        alt={datapoke.name} 
+        width="200"
+      />
+
+        <p>{datapoke.name}</p>
+        <p>{datapoke.id}</p>
+        <p>Altura: {datapoke.height/ 10} m / Peso: {datapoke.weight/ 10} km</p>
+
+
+   
+        <button onClick={toggleFavorito}>
+          {esFavorito ? '❤️' : '🤍'}
+        </button>
+
+    
+    </div>
+    <h1>{datapoke.height}</h1>
     </>
   )
 }
